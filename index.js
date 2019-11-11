ad4e23f4e825fd04a9c275db1a4cc8269c6d36e2
Milo Rastgoo <hello@informagician.com>
2019-11-11 16:57:11 -0500

    refactor to arrow function

--- index.js
+++ index.js
@@ -256,8 +256,19 @@ function getOlderCars(inventory,max) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  const qty = inventory.length;
+  const arr = [];
+  for (let i=0 ; i < qty ; i++) {
+    if (inventory[i].car_make == "Audi" /* || inventory[i].car_make == "Mercedes-Benz" || inventory[i].car_make == "Volkswagen" */) {
+      arr.push(inventory[i]);
+    } else if (inventory[i].car_make == "Mercedes-Benz") {
+      arr.push(inventory[i]);
+    } else if (inventory[i].car_make == "Volkswagen") {
+      arr.push(inventory[i]);
+    }
+  }
+  return arr;
 }
 
 /**
@@ -278,9 +289,9 @@ function getGermanCars(/* code here */) {
  *   return num * 2
  * }
 */
-const sum = null; // code here!
-const addFive = null; // code here!
-const argTimesTwo = null; // code here!
+const sum = (a,b) => { return a +b;}
+const addFive = (num) => { return num + 5;}
+const argTimesTwo = (num) => { return num * 2;}
 
 /**
  * ### Challenge `carMaker`
